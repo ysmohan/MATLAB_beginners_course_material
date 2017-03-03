49b63c0d7eacdace40e5cd8a0357cccdfb27142c
ysmohan <ysmohan@student.unimelb.edu.au>
2017-03-03 23:59:52 +1100

    Add files via upload

---
+++ Challenges/Loops_challenge.m
@@ -0,0 +1,15 @@
+%% Loops Challenge
+
+% Using the worked example as a guide, write a script that will calculate
+% the maximum of mass of the meteorite hitting each continent and display it.
+
+% Calculate the minimum and maximum for all the continents with odd
+% indices.
+% Change your loop so that you only display the minimum and maximum for
+% continents 1,2,4 and 7.
+
+%% Extension Challenge
+
+% Use a for loop to loop through each continent and generate a scatter plot
+% of the latitude v longitude on the same plot. Set the size of each of the
+% data point as mass/10000.
